36e81aa782cc3a19a8c3e29b15c5772aaf8e54d0
Edvid <spacemastr@gmail.com>
2024-01-19 01:55:36 +0100

    renamed a few things in netrw better keybinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -834,17 +834,18 @@ vim.keymap.set('n', '<leader>e', [[:Explore<CR>]], {desc = [[[E]xplore from here
 -- better keybinds for netrw
 vim.api.nvim_create_autocmd('filetype', {
   pattern = 'netrw',
-  desc = 'Better mappings for netrw',
+  desc = 'Better keybinds for netrw',
   callback = function()
-    local bind = function(lhs, rhs)
-      vim.keymap.set('n', lhs, rhs, {remap = true, buffer = true})
-    end 
+    local bind = function(new, old)
+      vim.keymap.set('n', new, old, {remap = true, buffer = true})
+    end
 
     -- edit new file
     bind('n', '%')
 
     -- rename file
     bind('r', 'R')
+
   end
 })
 
