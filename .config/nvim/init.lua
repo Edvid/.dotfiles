413726a05ec51ccbafc978f2c58383be96eb8558
Edvid <spacemastr@gmail.com>
2024-01-29 03:32:17 +0100

    abort function in nvim-cmp added back in
    actually super useful when filling in snippet stuff with
    TAB and I need cmp to be quiet for a moment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -818,7 +818,8 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<C-space>'] = cmp.mapping.complete(),
+    ['<C-c>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
     },
