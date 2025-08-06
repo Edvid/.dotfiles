0389657f124384c40ccd0976acf0ab60f6027dfb
Edvid <spacemastr@gmail.com>
2025-08-06 02:53:08 +0200

    feat(systempackages): added mouseless

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -135,6 +135,7 @@ in
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neovim
     wget
+    mouseless
     tmux
     gcc
     nodejs_22
