3c7b123a2d301f517ed2d9d69d3ce42af9416bd4
Edvid <spacemastr@gmail.com>
2025-08-06 02:49:07 +0200

    feat(nerdfonts): added jetbrains mono

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -45,6 +45,13 @@
     LC_TIME = "da_DK.UTF-8";
   };
 
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+    ];
+  };
+
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
