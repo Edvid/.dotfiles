a5075ad3b59ca3dd57e172600dda2e5f4b2854ba
Edvid <spacemastr@gmail.com>
2025-09-11 01:34:04 +0200

    feat(alacritty opacity toggle): added to .dotfiles
    I've had this script around for a while, but now it will be in my
    dotfiles

---
+++ alacritty-opacity-toggle.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+## If alacritty.yml does not exist, raise an alert
+[[ ! -f ~/.config/alacritty/alacritty.toml ]] && \
+    notify-send "alacritty.toml does not exist" && exit 0
+
+## Fetch background_opacity from alacritty.yml
+opacity=$(awk '$1 == "opacity" {print $3; exit}' \
+    ~/.config/alacritty/alacritty.toml)
+
+## Assign toggle opacity value
+case $opacity in
+  1)
+    toggle_opacity=0
+    ;;
+  *)
+    toggle_opacity=1
+    ;;
+esac
+
+## Replace opacity value in alacritty.yml
+sed -i -- "s/opacity = $opacity/opacity = $toggle_opacity/" \
+    ~/.config/alacritty/alacritty.toml
