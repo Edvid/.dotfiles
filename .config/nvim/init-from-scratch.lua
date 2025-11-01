2e83b5f333a6e8a6e3d934c8ad7fca2c597e11da
Edvid <spacemastr@gmail.com>
2025-11-01 12:23:35 +0100

    fix(init from scratch): nono, this is a setup I wrote myself
    only removed comments

--- .config/nvim/init-from-scratch.lua
+++ .config/nvim/init-from-scratch.lua
@@ -736,7 +736,6 @@ require("lazy").setup({
       vim.cmd.colorscheme "gruvbox"
     end,
   },
-  -- FIX: reduce
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
@@ -765,12 +764,6 @@ require("lazy").setup({
         lualine_z = { 'filetype' }
       }
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
   {
     -- Add indentation guides even on blank lines
